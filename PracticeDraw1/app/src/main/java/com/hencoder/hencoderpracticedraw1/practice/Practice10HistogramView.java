6a8a553d9dab9835d715d1c482083310051bf17d
dreamfish <cmy797@126.com>
2017-09-24 18:18:30 +0800

    画直方图

--- PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,11 +2,18 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+
 public class Practice10HistogramView extends View {
+    Paint paint1;
+    Paint paint2;
+    private final int PADDING = 10;//间隔
+    private final int WIDTH_ITEM = 80;//宽度
 
     public Practice10HistogramView(Context context) {
         super(context);
@@ -14,6 +21,14 @@ public class Practice10HistogramView extends View {
 
     public Practice10HistogramView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        paint1 = new Paint();
+        paint1.setColor(Color.GREEN);
+        paint1.setStrokeWidth(3);
+        paint1.setStyle(Paint.Style.FILL);
+
+        paint2 = new Paint();
+        paint2.setColor(Color.WHITE);
+        paint2.setStrokeWidth(3);
     }
 
     public Practice10HistogramView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -24,7 +39,26 @@ public class Practice10HistogramView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        综合练习
-//        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        //        综合练习
+        //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+
+        int[] heights = {5, 30, 25, 150, 200, 250, 125};
+        String[] lables = {"Froyo", "GB", "ICS", "JB", "KitKat", "L", "M"};
+
+        canvas.drawLine(width / 10, height * 0.8f, width / 10, height * 0.1f, paint2);
+        canvas.drawLine(width / 10, height * 0.8f, width * 0.9f, height * 0.8f, paint2);
+
+        int lastWidth = width / 10 + PADDING;
+
+        for (int i = 0; i < heights.length; i++) {
+            canvas.drawRect(lastWidth, height * 0.8f - heights[i], lastWidth + WIDTH_ITEM, height * 0.8f, paint1);
+            canvas.drawText(lables[i], lastWidth + 30, height * 0.8f + 20, paint2);
+            lastWidth = lastWidth + WIDTH_ITEM + PADDING;
+        }
+
     }
 }
